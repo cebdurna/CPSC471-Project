188bdeb1ad815ad98e24ebe139c3a6b2bfc5e7da
Joseph Besenski <josephdbesenski@gmail.com>
2022-04-05 19:20:55 -0600

    Add files via upload

---
+++ employee-login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Hotel Service</title>
+</head>
+
+<body>
+
+<a href="landing.php">Return to Landing Page</a>
+
+<hr />
+
+<H2>Employee Login</H2>
+
+<label>Username</label>
+<input type="text" name="username" placeholder = "Username"><br>
+
+<label>Password</label>
+<input type="password" name="password" placeholder="Password"><br>
+<button type="submit">Login</button>
+
+</body>
+
+</html>
