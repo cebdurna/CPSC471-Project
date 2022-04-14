6be3254ea9065179d069557bc8ba3b07847a0396
cebdurna <94244938+cebdurna@users.noreply.github.com>
2022-04-13 20:07:11 -0600

    Invoices Employee

--- hotel_FE/Invoices.php
+++ hotel_FE/Invoices.php
@@ -1,169 +1,66 @@
-<!DOCTYPE html>
 <html>
 	<body>
-		<p style="text-align: left;"><a title="placeholder link" href="placeholder%10link">Home Page</a>&emsp;&emsp;<a title="placeholder link" href="placeholder%10link">Employee Dashboard</a> <span style="float: right;"><a title="placeholderURL" href="placeholderURL">Logged in as Employee1</a></span>
-		</p>
-		<table style="border-collapse: collapse; width: 100%; height: 310px;" border="1">
-			<tbody>
-				<tr style="height: 66px;">
-					<th style="width: 20%; text-align: center; height: 66px;">
-					<h3>Invoice</h3>
-					</th>
-					<td style="width: 10%; text-align: center; height: 66px;">
-					<h3>Invoice ID</h3>
-					</td>
-					<td style="width: 10%; height: 66px; text-align: center;">
-					<h3 style="text-align: center;">Format</h3>
-					</td>
-					<td style="width: 10%; height: 66px; text-align: center;">
-					<h3 style="text-align: center;">Date Created</h3>
-					</td>
-					<td style="width: 10%; text-align: center; height: 66px;">
-					<h3 style="text-align: center;">Due Date</h3>
-					</td>
-					<td style="width: 10%; text-align: center; height: 66px;">
-					<h3 style="text-align: center;">Booking No</h3>
-					</td>
-					<td style="width: 10%; text-align: center; height: 66px;">
-					<h3 style="text-align: center;">Total Amount</h3>
-					</td>
-					<td style="width: 10%; text-align: center; height: 66px;">
-					<h3 style="text-align: center;">Paid Amount</h3>
-					<!-- </td>
-					<td style="width: 10%; text-align: center; height: 66px;">
-					<h3 style="text-align: center;">Edit</h3>
-					</td> -->
-				</tr>
-				<tr style="height: 51px;">
-					<td style="width: 20%; height: 51px; text-align: center;">
-					<h3><a title="placeHolder" href="placeHolder">Invoice1</a></h3>
-					</td>
-					<td style="width: 10%; height: 51px; text-align: center;">0100440</td>
-					<td style="width: 10%; height: 51px; text-align: center;">E-mail</td>
-					<td style="width: 10%; height: 51px; text-align: center;">March 28th, 1022&nbsp;</td>
-					<td style="width: 10%; height: 51px; text-align: center;">March 30th, 1022&nbsp;</td>
-					<td style="width: 10%; text-align: center; height: 51px;">
-					<p style="text-align: center;">12424</p>
-					</td>
-					<td style="width: 10%; text-align: center; height: 51px;">
-					<p style="text-align: center;">$1050</p>
-					</td>
-					<td style="width: 10%; text-align: center; height: 51px;">
-					<p style="text-align: center;">$900</p>
-					<!-- </td>
-					<td style="width: 10%; text-align: center; height: 51px;">
-					<p style="text-align: center;"><a title="placeHolder " href="placeHolder%10">Edit</a></p>
-					</td> -->
-				</tr>
-				<tr style="height: 51px;">
-					<td style="width: 20%; height: 51px; text-align: center;">
-					<h3><a title="placeHolder" href="placeHolder">Invoice2</a></h3>
-					</td>
-					<td style="width: 10%; height: 51px; text-align: center;">0432402</td>
-					<td style="width: 10%; height: 51px; text-align: center;">Physical</td>
-					<td style="width: 10%; height: 51px; text-align: center;">March 29th, 1022&nbsp;</td>
-					<td style="width: 10%; height: 51px; text-align: center;">April 1st, 1022&nbsp;</td>
-					<td style="width: 10%; text-align: center; height: 51px;">
-					<p style="text-align: center;">45414</p>
-					</td>
-					<td style="width: 10%; text-align: center; height: 51px;">
-					<p style="text-align: center;">$250</p>
-					</td>
-					<td style="width: 10%; text-align: center; height: 51px;">
-					<p style="text-align: center;">$250</p>
-					<!-- </td>
-					<td style="width: 10%; text-align: center; height: 51px;">
-					<p style="text-align: center;"><a title="placeHolder " href="placeHolder%10">Edit</a></p>
-					</td> -->
-				</tr>
-				<tr style="height: 51px;">
-					<td style="width: 20%; height: 51px; text-align: center;">
-					<h3><a title="placeHolder" href="placeHolder">Invoice3</a></h3>
-					</td>
-					<td style="width: 10%; height: 51px; text-align: center;">3104746</td>
-					<td style="width: 10%; height: 51px; text-align: center;">Physical</td>
-					<td style="width: 10%; height: 51px; text-align: center;">March 26th, 1022&nbsp;</td>
-					<td style="width: 10%; height: 51px; text-align: center;">March 28th, 1022&nbsp;</td>
-					<td style="width: 10%; text-align: center; height: 51px;">
-					<p style="text-align: center;">24689</p>
-					</td>
-					<td style="width: 10%; text-align: center; height: 51px;">
-					<p style="text-align: center;">$150</p>
-					</td>
-					<td style="width: 10%; text-align: center; height: 51px;">
-					<p style="text-align: center;">$150</p>
-					</td>
-					<!-- <td style="width: 10%; text-align: center; height: 51px;">
-					<p style="text-align: center;"><a title="placeHolder " href="placeHolder%10">Edit</a></p>
-					</td> -->
-				</tr>
-				<tr style="height: 51px;">
-					<td style="width: 20%; height: 51px; text-align: center;">
-					<h3><a title="placeHolder" href="placeHolder">Invoice4</a></h3>
-					</td>
-					<td style="width: 10%; height: 51px; text-align: center;">5467343</td>
-					<td style="width: 10%; height: 51px; text-align: center;">Physical</td>
-					<td style="width: 10%; height: 51px; text-align: center;">March 10th, 1022&nbsp;</td>
-					<td style="width: 10%; height: 51px; text-align: center;">March 13th, 1022&nbsp;</td>
-					<td style="width: 10%; text-align: center; height: 51px;">
-					<p style="text-align: center;">97847</p>
-					</td>
-					<td style="width: 10%; text-align: center; height: 51px;">
-					<p style="text-align: center;">$500</p>
-					</td>
-					<td style="width: 10%; text-align: center; height: 51px;">
-					<p style="text-align: center;">$500</p>
-					</td>
-					<!-- <td style="width: 10%; text-align: center; height: 51px;">
-					<p style="text-align: center;"><a title="placeHolder " href="placeHolder%10">Edit</a></p>
-					</td> -->
-				</tr>
-				<tr style="height: 53px;">
-					<td style="width: 20%; height: 53px; text-align: center;">
-					<h3><a title="placeHolder" href="placeHolder">Invoice5</a></h3>
-					</td>
-					<td style="width: 10%; height: 53px; text-align: center;">1267839</td>
-					<td style="width: 10%; height: 53px; text-align: center;">E-mail</td>
-					<td style="width: 10%; height: 53px; text-align: center;">March 27th, 1022&nbsp;</td>
-					<td style="width: 10%; height: 53px; text-align: center;">April 2nd, 1022&nbsp;</td>
-					<td style="width: 10%; text-align: center; height: 53px;">
-					<p style="text-align: center;">25413</p>
-					</td>
-					<td style="width: 10%; text-align: center; height: 53px;">
-					<p style="text-align: center;">$390</p>
-					</td>
-					<td style="width: 10%; text-align: center; height: 53px;">
-					<p style="text-align: center;">$0</p>
-					</td>
-					<!-- <td style="width: 10%; text-align: center; height: 53px;">
-					<p style="text-align: center;"><a title="placeHolder " href="placeHolder%10">Edit</a></p>
-					</td> -->
-				</tr>
-			</tbody>
-		</table>
-		<p>&nbsp;</p>
-		<table style="border-collapse: collapse; width: 100%; height: 53px;" border="1">
-			<tr style="height: 53px;">
-				<td style="width: 20%; height: 53px; text-align: center;">
-				<h3>New Invoice</h3>
+		<?php
+            echo '<p style="text-align: left;">';
+            echo '<a href="landingPage.php">Landing Page</a>';
+			echo '&emsp;&emsp;<a href="hotelemployeep.php">Employee Dashboard</a>';
+            echo '<span style="float: right;">';
+            echo '<a href="hotelemployeep.php">Logged in as'. $_COOKIE["userName"] . '</a>&nbsp; &nbsp; &nbsp';
+            echo '<a href="logoutlink">Logout</a>';
+            echo '</span>';
+            echo '</p>';
+			echo "<br><h1>Invoice Detail</h1><br>";
+
+			echo "<table border='1'> <tr>
+				<th>Invoice ID</th>
+				<th>Format</th>
+				<th>Date Created</th>
+				<th>Date Due</th>
+				<th>Booking No</th>
+				<th>Total</th>
+				<th>Total Paid</th>
+				</tr>";
+
+			$url = "http://localhost:8000/employee/invoice";
+			$curl = curl_init($url);
+			curl_setopt($curl, CURLOPT_URL, $url);
+			curl_setopt($curl, CURLOPT_GET, 1);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+			$response = curl_exec($curl);
+			curl_close($curl);
+
+			$response = json_decode($response);
+        	foreach ($response as $invoice){
+				$Invoice_ID =  $invoice->Invoice_ID;
+				$Format =  $invoice->Format;
+				$Date_created =  $invoice->Date_created;
+				$Date_due =  $invoice->Date_due;
+				$booking_no =  $invoice->booking_no;
+				$total =  $invoice->total;
+				$total_paid =  $invoice->total_paid;
+				echo "<tr>
+				<td>" . $Invoice_ID . "</td>
+				<td>" . $Format . "</td>
+				<td>" . $Date_created . "</td>
+				<td>" . $Date_due . "</td>
+				<td>" . $booking_no . "</td>
+				<td>" . $total . "</td>
+				<td>" . $total_paid . "</td>
+				<td>
+					<form action='InvoiceDetail.php' method='post'>
+						<input type='hidden' name='Invoice_ID' value = '".$Invoice_ID."'>
+						<input type='hidden' name='Format' value = '". $Format ."'>
+						<input type='hidden' name='Date_created' value = '" . $Date_created ."'>
+						<input type='hidden' name='Date_due' value = '" . $Date_due . "'>
+						<input type='hidden' name='booking_no' value = '" . $booking_no . "'>
+						<input type='hidden' name='total' value = '" . $total . "'>
+						<input type='hidden' name='total_paid' value = '" . $total_paid . "'>
+						<input type='submit' value='Edit'>
+					</form>
 				</td>
-				<td style="width: 10%; height: 53px; text-align: center;"></td>
-				<td style="width: 10%; height: 53px; text-align: center;"></td>
-				<td style="width: 10%; height: 53px; text-align: center;">&nbsp;</td>
-				<td style="width: 10%; height: 53px; text-align: center;">&nbsp;</td>
-				<td style="width: 10%; text-align: center; height: 53px;">
-				<p style="text-align: center;"></p>
-				</td>
-				<td style="width: 10%; text-align: center; height: 53px;">
-				<p style="text-align: center;"></p>
-				</td>
-				<td style="width: 10%; text-align: center; height: 53px;">
-				<p style="text-align: center;"></p>
-				</td>
-				<td style="width: 10%; text-align: center; height: 53px;">
-				<p style="text-align: center;"><a title="placeHolder " href="placeHolder%10">Create a new Invoice</a></p>
-				</td>
-			</tr>
-		</table>
-  	</body>
+				</tr>";
+			}
+		?>
+
+	</body>
 </html>
